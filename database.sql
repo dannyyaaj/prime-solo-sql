fee23506fd074cf52fa65d065014531b914e769d
Danny Yang <dannyyaaj@gmail.com>
2018-07-12 11:01:57 -0500

    completed classroom SQL practice

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+
 	-- 1.	Get all users from Chicago.
 	SELECT "username" FROM "accounts"
 	WHERE "city" LIKE 'chicago';
@@ -18,3 +19,20 @@
 	SELECT "username", "account_balance" FROM "accounts"
 	WHERE "account_balance" BETWEEN 0 AND (SELECT MAX("account_balance") from "accounts")
 	ORDER BY "account_balance" DESC LIMIT 3;
+	
+	-- 6.	Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+	SELECT "username", "account_balance" FROM "accounts"
+	WHERE "account_balance" >= (SELECT MIN("account_balance") from "accounts")
+	ORDER BY "account_balance" LIMIT 3;
+	
+	-- 7.	Get all users with account balances that are more than $100.
+	SELECT * FROM "accounts"
+	WHERE "account_balance" > 100;
+	
+	-- 8.	Add a new record.
+	INSERT INTO "accounts" (username, city, transactions_completed, transactions_attempted, account_balance)
+   	VALUES ('justine','minneapolis',3,10,2500.00);
+	
+	-- 9.	The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+	SELECT * FROM "accounts"
+	WHERE ("city" LIKE 'miami' OR "city" LIKE 'phoenix') AND "transactions_completed" < 5;
